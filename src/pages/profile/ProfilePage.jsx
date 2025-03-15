9fa6d489d89178356ab5c9385d2ca8d20db95067
ashokbasak <ashokbasak80@gmail.com>
2025-03-15 18:44:34 +0530

    some changes

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,8 +1,42 @@
+import Chat from '../../components/chat/Chat'
+import List from '../../components/list/List'
 import './profilePage.scss'
 
 function ProfilePage(){
   return (
-    <div className='profilePage'>ProfilePage</div>
+    <div className='profilePage'>
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>Profile</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              <img src="https://e7.pngegg.com/pngimages/81/570/png-clipart-profile-logo-computer-icons-user-user-blue-heroes-thumbnail.png" alt="" />
+              <span>John</span>
+            </span>
+            <span>
+               <b>john@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat />
+        </div>
+      </div>
+    </div>
   )
 }
 
